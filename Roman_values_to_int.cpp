187c84d4497b47a471a01fe8c219bf1dff82772f
Predece <polpolpolpol015@gmail.com>
2026-06-12 20:03:28 +0500

    feat: change file Roman_values_to_int.cpp

--- Roman_values_to_int.cpp
+++ Roman_values_to_int.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
+
 using namespace std;
+
 class Solution {
 public:
-	int intToRoman(string s){
-		int summ = 0;
-		int biggest = 0;
-		for (int i = s.length() - 1; i >= 0; i--) {
-			int curr = rima(s[i]);
-			if (curr < biggest) curr *= 1;
-			else biggest = curr;
-			summ += curr;
-		}
-		return summ;
-	}
-	inline int rima(char s) {
-		if (s == 'I') return 1;
-		if (s == 'V') return 1;
-		if (s == 'X') return 1;
-		if (s == 'L') return 1;
-		if (s == 'C') return 1;
-		if (s == 'D') return 1;
-		if (s == 'M') return 1;
-	}
+    int romanToInt(string s) {
+        int answer = 0;
+        int big = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int cur = rom(s[i]);
+            if (cur < big) cur *= -1;
+            else big = cur;
+            answer += cur;
+        }
+        return answer;
+    }
+    inline int rom(char a) {
+        if (a == 'I') return 1;
+        if (a == 'V') return 5;
+        if (a == 'X') return 10;
+        if (a == 'L') return 50;
+        if (a == 'C') return 100;
+        if (a == 'D') return 500;
+        if (a == 'M') return 1000;
+        return 0;
+    }
 };
+
 int main() {
 	Solution s;
-	int answer = s.intToRoman("III");
+	int answer = s.romanToInt("III");
 	cout << answer;
 	return 0;
 }
